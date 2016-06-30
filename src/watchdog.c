4cc6ee12c692e84c0801bb068e85888d2c6bc613
Gonzalo Casas <gonzalo.casas@local.ch>
2016-07-01 01:50:31 +0200

    signal handling funcs

--- src/watchdog.c
+++ src/watchdog.c
@@ -2,19 +2,36 @@
  Watchdog service that monitors unintended resets of the SX1301.
  */
 
-#include <stdint.h>				/* C99 types */
-#include <stdbool.h>			/* bool type */
-#include <stdio.h>				/* printf, fprintf, snprintf, fopen, fputs */
+#include <stdint.h>					/* C99 types */
+#include <stdbool.h>				/* bool type */
+#include <stdio.h>					/* printf, fprintf, snprintf, fopen, fputs */
 
-#include <signal.h>				/* sigaction */
-#include <time.h>				/* time, clock_gettime, strftime, gmtime */
-#include <sys/time.h>			/* timeval */
-#include <stdlib.h>				/* atoi, exit */
-#include <errno.h>				/* error messages */
+#include <signal.h>					/* sigaction */
+#include <time.h>					/* time, clock_gettime, strftime, gmtime */
+#include <sys/time.h>				/* timeval */
+#include <stdlib.h>					/* atoi, exit */
+#include <errno.h>					/* error messages */
 
 #include "loragw_hal.h"
 
-#define WATCHDOG_INTERVAL 5	/* default time interval for statistics */
+#define WATCHDOG_INTERVAL 5			/* default watchdog time interval in seconds */
+
+/* Signal handling variables */
+volatile bool exit_sig = false;		/* 1 -> application terminates cleanly (shut down hardware, close open files, etc) */
+volatile bool quit_sig = false;		/* 1 -> application terminates without shutting down the hardware */
+
+/* Function declarations */
+static void sig_handler(int sigio);
+
+/* Function definitions */
+static void sig_handler(int sigio) {
+	if (sigio == SIGQUIT) {
+		quit_sig = true;;
+	} else if ((sigio == SIGINT) || (sigio == SIGTERM)) {
+		exit_sig = true;
+	}
+	return;
+}
 
 
 int main(void)
